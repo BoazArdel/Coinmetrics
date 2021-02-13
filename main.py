55ab7d82418f13ab6200a096031d95009364040e
Ardel <boazardel@gmail.com>
2021-02-14 00:40:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ with open('data6.txt', 'w') as outfile:
     json.dump(mydata, outfile, indent=4, sort_keys=True, default=str)
 
 exporter.export_to_excel(mydata,["interval_id", "year", "day", "avg_sec_bs", "avg_am_bs", "avg_pr_bs", "min_pr_bs", "max_pr_bs", "val_bs", "avg_sec_kr", "avg_am_kr", "avg_pr_kr", "min_pr_kr", "max_pr_kr", "val_kr", "avg_sec_bf", "avg_am_bf", "avg_pr_bf", "min_pr_bf", "max_pr_bf", "val_bf", "avg_sec_cb", "avg_am_cb", "avg_pr_cb", "min_pr_cb", "max_pr_cb", "val_cb"])
-exporter.xslx_to_dta(input("without suffix, your excel name:"))
+#exporter.xslx_to_dta(input("without suffix, your excel name:"))
