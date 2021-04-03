8349ef2ba6cd3c865bace88204162d485e6d67b9
Ardel <boazardel@gmail.com>
2021-04-03 16:32:26 +0300

    VWAP and amounts
    adding volume wighted average prices and amounts per interval per exchange

--- miner.py
+++ miner.py
@@ -1,9 +1,11 @@
 import requests
 import json
 from passwords import API_KEY
+import datetime
 
 ########## API extractor ###############
-def getDataByEndDate(url,selected_date):
+def getDataByEndDate(url,selected_date,output_file):
+    
     data = []
     response = requests.get(url).json()
     for i in range(0,10000): 
@@ -11,8 +13,9 @@ def getDataByEndDate(url,selected_date):
         #print(json.dumps(response, indent=4, sort_keys=True)) <if DEBUG>
         
         for datagram in response["data"]:
-                data.append({"timestamp": datagram["time"], "amount": datagram["amount"], "price": datagram["price"], "market": datagram["market"]}) #to add 'side'
-        
+                obs = {"timestamp": datagram["time"], "amount": datagram["amount"], "price": datagram["price"], "market": datagram["market"]}
+                data.append(obs) #to add 'side'
+                output_file.write(str(obs))
         response = requests.get(response["next_page_url"]).json()
         print(str(i) + ":" + str(response["data"][0]["time"]))
 
